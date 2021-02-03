0e71342ad4486fb3784fcc6c7f0e5a4ec9edd061
Taha Burak <73062498+TBurak96@users.noreply.github.com>
2021-02-03 20:25:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new CarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Çıkış senesi: " + car.ModelYear+"Açıklama: "+car.Description);
+            }
+        }
+    }
+}
